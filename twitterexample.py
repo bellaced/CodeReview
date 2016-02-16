68f66a748bea08b8822474c4cebe8785dbb540d6
Michael Fahy <fahy@chapman.edu>
2016-02-15 20:01:29 -0800

    updates twitterexample.py

--- twitterexample.py
+++ twitterexample.py
@@ -1,9 +1,15 @@
+# twitterexaqmple.py
+# Demonstrates connecting to the twitter API and accessing the twitter stream
+# Author: Michael Fahy
+# Version 1.1
+# Date: February 15, 2016
+
 import twitter
 
 # XXX: Go to http://dev.twitter.com/apps/new to create an app and get values
 # for these credentials, which you'll need to provide in place of these
 # empty string values that are defined as placeholders.
-# See https://dev.twitter.com/docs/auth/oauth for more information 
+# See https://dev.twitter.com/docs/auth/oauth for more information
 # on Twitter's OAuth implementation.
 
 print 'Example 1'
@@ -70,17 +76,16 @@ raw_input("Press Enter to see Example 4 ")
 print
 print "---------------------------------------------------------------------"
 print 'Example 4. Computing the intersection of two sets of trends'
-world_trends_set = set([trend['name'] 
+world_trends_set = set([trend['name']
                         for trend in world_trends[0]['trends']])
 
-us_trends_set = set([trend['name'] 
-                     for trend in us_trends[0]['trends']]) 
+us_trends_set = set([trend['name']
+                     for trend in us_trends[0]['trends']])
 
 common_trends = world_trends_set.intersection(us_trends_set)
 
 print common_trends
 print
-raw_input("Press Enter to see Example 5 ")
 print
 print "---------------------------------------------------------------------"
 print 'Example 5. Collecting search results'
@@ -90,7 +95,7 @@ print
 # Import unquote to prevent url encoding errors in next_results
 from urllib import unquote
 
-# XXX: Set this variable to a trending topic, 
+# XXX: Set this variable to a trending topic,
 # or anything else for that matter. The example query below
 # was a trending topic when this content was being developed
 # and is used throughout the remainder of this chapter.
@@ -99,7 +104,7 @@ from urllib import unquote
 q = raw_input('Enter a search term: ')
 
 #print q
-#raw_input("Press Enter to continue") 
+#raw_input("Press Enter to continue")
 
 count = 1000
 
@@ -118,11 +123,11 @@ for _ in range(5):
         next_results = search_results['search_metadata']['next_results']
     except KeyError, e: # No more results when next_results doesn't exist
         break
-        
+
     # Create a dictionary from next_results, which has the following form:
     # ?max_id=313519052523986943&q=NCAA&include_entities=1
     kwargs = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
-    
+
     search_results = twitter_api.search.tweets(**kwargs)
     statuses += search_results['statuses']
 
@@ -137,75 +142,44 @@ print 'Example 6. Extracting text, screen names, and hashtags from tweets'
 print
 raw_input("Press Enter to see Example 6 ")
 print
-status_texts = [ status['text'] 
+status_texts = [ status['text']
                  for status in statuses ]
 
-screen_names = [ user_mention['screen_name'] 
+screen_names = [ user_mention['screen_name']
                  for status in statuses
                      for user_mention in status['entities']['user_mentions'] ]
 
-hashtags = [ hashtag['text'] 
+hashtags = [ hashtag['text']
              for status in statuses
                  for hashtag in status['entities']['hashtags'] ]
 
 # Compute a collection of all words from all tweets
-words = [ w 
-          for t in status_texts 
+words = [ w
+          for t in status_texts
               for w in t.split() ]
 
 # Explore the first 5 items for each...
 
 print json.dumps(status_texts[0:5], indent=1)
-print json.dumps(screen_names[0:5], indent=1) 
+print json.dumps(screen_names[0:5], indent=1)
 print json.dumps(hashtags[0:5], indent=1)
 print json.dumps(words[0:5], indent=1)
+
+
 
 print
 raw_input("Press Enter to see Example 7 ")
 print
 print "---------------------------------------------------------------------"
-print 'Example 7. Creating a basic frequency distribution from the words in tweets'
-
-
-from collections import Counter
-
-for item in [words, screen_names, hashtags]:
-    c = Counter(item)
-    print c.most_common()[:10] # top 10
-    print
-
-print
-raw_input("Press Enter to see Example 8 ")
-print
-
-print "---------------------------------------------------------------------"
-print 'Example 8. Using prettytable to display tuples in a nice tabular format'
-
-
-from prettytable import PrettyTable
-
-for label, data in (('Word', words), 
-                    ('Screen Name', screen_names), 
-                    ('Hashtag', hashtags)):
-    pt = PrettyTable(field_names=[label, 'Count']) 
-    c = Counter(data)
-    [ pt.add_row(kv) for kv in c.most_common()[:10] ]
-    pt.align[label], pt.align['Count'] = 'l', 'r' # Set column alignment
-    print pt
-
-print
-raw_input("Press Enter to see Example 9 ")
-print
-print "---------------------------------------------------------------------"
-print 'Example 9. Calculating lexical diversity for tweets'
+print 'Example 7. Calculating lexical diversity for tweets'
 
 # A function for computing lexical diversity
 def lexical_diversity(tokens):
-    return 1.0*len(set(tokens))/len(tokens) 
+    return 1.0*len(set(tokens))/len(tokens)
 
 # A function for computing the average number of words per tweet
 def average_words(statuses):
-    total_words = sum([ len(s.split()) for s in statuses ]) 
+    total_words = sum([ len(s.split()) for s in statuses ])
     return 1.0*total_words/len(statuses)
 print 'Lexical diversity of words: '
 print lexical_diversity(words)
@@ -217,113 +191,26 @@ print 'Average number of words per tweet: '
 print average_words(status_texts)
 
 print
-raw_input("Press Enter to see Example 10 ")
+raw_input("Press Enter to see Example 8 ")
 print
-
 print "---------------------------------------------------------------------"
-print 'Example 10. Finding the most popular retweets'
-
-retweets = [
-            # Store out a tuple of these three values ...
-            (status['retweet_count'], 
-             status['retweeted_status']['user']['screen_name'],
-             status['text']) 
-            
-            # ... for each status ...
-            for status in statuses 
-            
-            # ... so long as the status meets this condition.
-                if status.has_key('retweeted_status')
-           ]
-
-# Slice off the first 5 from the sorted results and display each item in the tuple
-
-pt = PrettyTable(field_names=['Count', 'Screen Name', 'Text'])
-[ pt.add_row(row) for row in sorted(retweets, reverse=True)[:5] ]
-pt.max_width['Text'] = 50
-pt.align= 'l'
-print pt
-
-print
-raw_input("Press Enter to see Example 11 ")
-print
-
-print "---------------------------------------------------------------------"
-print 'Example 11. Looking up users who have retweeted a status'
+print 'Example 8. Looking up users who have retweeted a status'
 
 
-# Get the original tweet id for a tweet from its retweeted_status node 
+# Get the original tweet id for a tweet from its retweeted_status node
 # and insert it here in place of the sample value that is provided
 # from the text of the book
 
 _retweets = twitter_api.statuses.retweets(id=317127304981667841)
 print [r['user']['screen_name'] for r in _retweets]
 
-print
-raw_input("Press Enter to see Example 12 ")
-print
-
-print "---------------------------------------------------------------------"
-print 'Example 12. Plotting frequencies of words'
-import matplotlib.pyplot as plt
-word_counts = sorted(Counter(words).values(), reverse=True)
-
-plt.loglog(word_counts)
-plt.ylabel("Freq")
-plt.xlabel("Word Rank")
-plt.show()
 
 print
-raw_input("Press Enter to see Example 13 ")
+raw_input("Press Enter to see Example 9 ")
 print
 
 print "---------------------------------------------------------------------"
-print 'Example 13. Generating histograms of words, screen names, and hashtags'
-
-for label, data in (('Words', words), 
-                    ('Screen Names', screen_names), 
-                    ('Hashtags', hashtags)):
-
-    # Build a frequency map for each set of data
-    # and plot the values
-    c = Counter(data)
-    plt.hist(c.values())
-    
-    # Add a title and y-label ...
-    plt.title(label)
-    plt.ylabel("Number of items in bin")
-    plt.xlabel("Bins (number of times an item appeared)")
-    
-    # ... and display as a new figure
-    #plt.figure()
-    plt.show()
-
-print
-raw_input("Press Enter to see Example 14 ")
-print
-
-print "---------------------------------------------------------------------"
-print 'Example 14. Generating a histogram of retweet counts'
-
-
-# Using underscores while unpacking values in
-# a tuple is idiomatic for discarding them
-
-counts = [count for count, _, _ in retweets]
-
-plt.hist(counts)
-plt.title("Retweets")
-plt.xlabel('Bins (number of times retweeted)')
-plt.ylabel('Number of tweets in bin')
-plt.show()
-
-print counts
-print
-raw_input("Press Enter to see Example 15 ")
-print
-
-print "---------------------------------------------------------------------"
-print 'Example 15. Sentiment Analysis '
+print 'Example 9. Sentiment Analysis on the search term from Example 5'
 sent_file = open('AFINN-111.txt')
 
 scores = {} # initialize an empty dictionary
@@ -337,5 +224,3 @@ for word in words:
     if uword in scores.keys():
         score = score + scores[word]
 print float(score)
-
-
